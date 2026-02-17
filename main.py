a411b0bc2a02d11b95129e24702440e1ed71e12d
Ravi43chaurasiya <ravi8601150552@gmail.com>
2026-02-17 19:57:41 +0530

    working with json file and get method

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+import json
 
 app=FastAPI()
 
+def load_data():
+  with open('patients.json','r') as f:
+    data=json.load(f)
+  
+  return data
+
+
+
 @app.get("/")
 def hello():
-  return {'message':'Hello world.'}
+  return {'message':'Patient Management System API'}
 
 @app.get("/about")
 def about():
-  return {'message':'BinaryBrothers is an educational plateform where you can learn AI.'}
+  return {'message':'A fully functional API to manage your patient records'}
+
+@app.get('/view')
+def view():
+  data=load_data()
+
+  return data
